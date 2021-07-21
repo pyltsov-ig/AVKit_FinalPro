2eb20d380625647880f2c490c3fb7ddb2b848c8f
Igor Pyltsov <pyltsov@gmail.com>
2021-07-21 15:36:05 +0300

    Initial Commit

---
+++ AVKit_FinalPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVKit_FinalPro
+//
+//  Created by Igor Pyltsov on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
